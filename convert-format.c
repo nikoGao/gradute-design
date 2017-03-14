93edb447f729cd5d5b9cda83557f9ebfd44a5422
nagao <nagao@nagao.redhat.com>
2017-03-14 17:41:57 +0800

    Edit C file

---
+++ convert-format.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	FILE *fpIn,*fpOut;
+    short x;
+    fpIn = fopen("musicD_44p1_128bps.mp3.dec", "rb");
+    fpOut = fopen("pcmFile.pcm", "wb");
+    while( fread(&x, sizeof(short), 1, fpIn) == 1)
+    {
+    x = ((x>>8)&0xff)| ((x&0xff)<<8);
+    fwrite(&x, sizeof(short), 1, fpOut);
+    }
+    fclose(fpIn);
+    fclose(fpOut);
+	return 0;
+}
